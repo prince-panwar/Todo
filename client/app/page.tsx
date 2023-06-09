fdda59c685adc017e200c073d8b096c18a4fd706
Prince <princegujjar1.pp@gmail.com>
2023-06-09 22:36:49 +0530

    initial commit

---
+++ client/app/page.tsx
@@ -0,0 +1,14 @@
+import Header from "../components/Header"
+import TodoInput from "@/components/TodoInput"
+import styles from './page.module.css'
+
+export default function Home() {
+  return (
+    <main className={styles.main}>
+     <div className={styles.Header}>
+    <Header/>
+    <TodoInput/>
+    </div>
+    </main>
+  )
+}
---
+++ client/components/Header.tsx
@@ -0,0 +1,10 @@
+const Header= ()=>{
+    return(
+         <div>
+            <h1>
+                TODO-LIST
+            </h1>
+        </div>
+);
+}
+export default Header;
