083e30c708451a57118964d57c6d8cbaaa32bd91
Prince <princegujjar1.pp@gmail.com>
2023-06-16 22:10:11 +0530

    removed Reduc

--- client/app/layout.tsx
+++ client/app/layout.tsx
@@ -1,4 +1,5 @@
 import "./globals.css";
+
 export default function RootLayout({
   children,
 }: {
@@ -6,7 +7,9 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-    <body>{children}</body>
+    
+    <body> {children} </body>
+   
     </html>
   );
 
--- client/components/TodoInput.tsx
+++ client/components/TodoInput.tsx
@@ -1,16 +1,17 @@
 "use client"
-import React, { FormEvent, useState } from 'react';
+import React, {useState } from 'react';
 import { useDispatch } from 'react-redux';
-import {addNewTodo} from '../redux/actions'
+
 const TodoInput =()=>{
-    const [todo,setTodo]=useState<string>("");
+  const [todoItem,setTodoItem] = useState<string>("");
     const dispatch =useDispatch();
     const HandleSubmit=(e:  React.SyntheticEvent<HTMLFormElement>)=>{
         e.preventDefault();
         const form = e.target as HTMLFormElement;
         const input = form.elements.namedItem('userInput') as HTMLInputElement;
        console.log(input.value);
-      dispatch(addNewTodo(input.value));
+       setTodoItem(input.value);
+     
      }
      
     
