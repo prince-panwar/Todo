e9ae92a0db2f4b67906a70875c312e17665976f2
Prince <princegujjar1.pp@gmail.com>
2023-06-17 23:43:50 +0530

    todo list

--- server/routes/route.js
+++ server/routes/route.js
@@ -2,3 +2,4 @@ import  express  from "express";
 import { addNewTodo } from "../controller/todo-controller.js";
 export const route = express.Router();
 route.post('/todos',addNewTodo);
+route.get('/todos',getAllTodos);
