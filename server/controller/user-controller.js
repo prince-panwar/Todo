3d6b45d14c3a04a2beb77e699031bb235099897d
Prince <princegujjar1.pp@gmail.com>
2023-06-28 17:47:27 +0530

    validator installed

--- server/controller/user-controller.js
+++ server/controller/user-controller.js
@@ -2,9 +2,18 @@ import mongoose from "mongoose";
 import { user } from "../model/UserModel.js";
 
 export const loginUser = async (request, response) => {
+    
     response.json({ msg: 'login' });
+
 }
 
 export const  signUser= async(request,response)=>{
-    response.json({ msg: 'signup' });
+    const {email, password} = request.body;
+    try{
+     const User = await user.signup(email, password);
+     response.status(200).json({email,User})
+    }catch(e){
+ response.status(400).json(e.message)
+    }
+ 
 }
--- server/model/UserModel.js
+++ server/model/UserModel.js
@@ -1,5 +1,6 @@
 import mongoose from "mongoose";
 import bcrypt from "bcrypt";
+import validator from "validator";
 const Schema = mongoose.Schema
 
 const userSchema= new Schema({
@@ -17,16 +18,28 @@ const userSchema= new Schema({
 
 //static singup method
 
-userSchema.static.signup=async(email,password)=>{
+userSchema.statics.signup=async function(email,password){
+    //validation
+    if(!email || !password){
+        throw Error('All fields must be filled')
+    }
+    if(!validator.isEmail(email)){
+        throw Error('Please enter a valid email')
+}
+if(!validator.isStrongPassword(password)){
+    throw Error("Please enter a strog password")
+}
     const exists = await this.findOne({email})
     if(exists){
         throw Error('Email already exists')
         
     }
+const salt = await bcrypt.genSalt(10)
+const hash = await bcrypt.hash(password,salt)
 
+const user=await this.create({email,password:hash})
+
+return user
     
-
-
-       
 }
 export const user =mongoose.model('user',userSchema);
